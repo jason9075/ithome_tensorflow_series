f7948356b98c16a47caccaecb87902216bb38d7c
Jason Kuan (Astra) <jason9075@gmail.com>
2019-09-02 12:15:18 +0800

    update day9 code

--- 09/bn_and_dropout.py
+++ 09/bn_and_dropout.py
@@ -4,8 +4,8 @@ OUTPUT_PATH = '../events/'
 
 
 def main():
-    input_node = tf.placeholder(shape=[None, 100, 100, 3], dtype=tf.float32)
-    training_node = tf.placeholder(shape=(), dtype=tf.bool)
+    input_node = tf.placeholder(shape=[None, 100, 100, 3], dtype=tf.float32, name='input_node')
+    training_node = tf.placeholder(shape=(), dtype=tf.bool, name='training')
 
     net = tf.layers.conv2d(input_node, 32, (3, 3), strides=(2, 2), padding='same', name='conv_1')
     net = tf.layers.batch_normalization(net, training=training_node, name='bn')
@@ -15,7 +15,15 @@ def main():
 
     tf.identity(net, name='final')
 
-    tf.summary.FileWriter(OUTPUT_PATH, graph=tf.get_default_graph())
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        sess.run(tf.local_variables_initializer())
+
+        frozen_graph = tf.graph_util.convert_variables_to_constants(
+            sess, tf.get_default_graph().as_graph_def(), ['final'])
+
+        tf.summary.FileWriter(OUTPUT_PATH, graph=frozen_graph)
+        tf.io.write_graph(frozen_graph, "../pb/", "bn_dropout_model.pb", as_text=False)
 
 
 if __name__ == '__main__':
