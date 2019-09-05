8acc132fb5c31c8f7e0dddfef9e8ebbf0a3ea82f
Jason Kuan (Astra) <jason9075@gmail.com>
2019-09-05 15:45:49 +0800

    extract feature as function

--- 13/load_tfrecord.py
+++ 13/load_tfrecord.py
@@ -10,22 +10,35 @@ def main():
         example = tf.train.Example()
         example.ParseFromString(tf_record)
 
-        name = example.features.feature['member/name'].bytes_list.value[
-            0].decode('utf8')
-        encoded_image = example.features.feature[
-            'member/encoded'].bytes_list.value[0]
+        name = get_bytes(example, 'member/name').decode('utf8')
+        encoded_image = get_bytes(example, 'member/encoded')
         with tf.gfile.GFile(f'img/{name}.png', 'wb') as fid:
             fid.write(encoded_image)
 
-        age = example.features.feature['member/age'].int64_list.value[0]
-        height = example.features.feature['member/height'].float_list.value[0]
-        prefer_prods = example.features.feature[
-            'member/prefer_prods'].int64_list.value
+        age = get_int64(example, 'member/age')
+        height = get_float(example, 'member/height')
+        prefer_prods = get_int64_list(example, 'member/prefer_prods')
 
         print(f'user: {name}')
         print(f'age: {age}')
-        print(f'height: {height}')
+        print(f'height: {height:.1f}')
         print(f'prefer_prdos: {prefer_prods}\n')
+
+
+def get_int64(example, key):
+    return example.features.feature[key].int64_list.value[0]
+
+
+def get_int64_list(example, key):
+    return example.features.feature[key].int64_list.value
+
+
+def get_bytes(example, key):
+    return example.features.feature[key].bytes_list.value[0]
+
+
+def get_float(example, key):
+    return example.features.feature[key].float_list.value[0]
 
 
 if __name__ == '__main__':
